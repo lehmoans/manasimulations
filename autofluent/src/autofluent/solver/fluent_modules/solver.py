800d3430c5d2f167e914dd502b75d04deb7a9b0b
sai <madanawas@gmail.com>
2026-09-23 15:10:34 +1000

    align Fluent module stages with fixed workflow

--- autofluent/src/autofluent/solver/fluent_modules/solver.py
+++ autofluent/src/autofluent/solver/fluent_modules/solver.py
@@ -1,41 +1,62 @@
-# importing fluent classes
+from .post_process.post_process import Post_Process
 from .setup.setup import Setup
 from .solution.solution import Solution
-from .post_process.post_process import Post_Process
+
 
 class Solver:
-    def __init__(self,session, config):
+
+    def __init__(self, session, config):
         self.fluent_setup = Setup()
         self.solution = Solution()
-        self.run_post_process= Post_Process()
-        
+        self.run_post_process = Post_Process()
+
         self.config = config
         self.session = session
 
     def setup(self):
-        #setup
-        self.fluent_setup.setup(self.session, self.config)
-        self.solution.setup(self.session, self.config["solution"])
+        setup_config = self.config.get("setup", self.config)
+
+        self.fluent_setup.setup(self.session, setup_config)
+        return True
+
+    def setup_solution(self):
+        solution_config = self.config.get("solution", {})
+        self.solution.setup(self.session, solution_config)
         return True
 
     def solve(self):
-        self.session.settings.solution.run_calculation.iterate(iter_count= self.config["solver"]["run_calc_settings"]["iter_count"])
-    
+        iter_count = (
+            self.config.get("run_calc_settings", {})
+            .get("iter_count", 0)
+        )
+
+        self.session.settings.solution.run_calculation.iterate(
+            iter_count=iter_count
+        )
+        return {
+            "status": "success",
+            "iterations": iter_count,
+        }
+
     def post_process(self):
-        if self.config["save"]["enabled"]:
+        save_config = self.config.get("save", {})
+
+        if save_config.get("enabled", False):
             self.run_post_process.setup(self.session, self.config)
 
+        return True
+
     def save(self):
-        self.session.settings.file.write_case_data(
-        file_name=self.config["save"]["name"])
+        save_config = self.config.get("save", {})
+        file_name = save_config.get("name") or "autofluent_result.cas.h5"
+
+        return self.session.settings.file.write_case_data(
+            file_name=file_name
+        )
 
     def run(self):
         self.setup()
+        self.setup_solution()
         self.solve()
         self.post_process()
-        self.save()
-
-
-        
-    
-        
+        return self.save()
