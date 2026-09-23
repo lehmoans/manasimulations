4cd41ba8985899ac38ff5c892fc9be305240914e
sai <madanawas@gmail.com>
2026-09-24 04:21:07 +1000

    Integrate solver modules with case YAML

--- autofluent/src/autofluent/solver/fluent_modules/solution/methods.py
+++ autofluent/src/autofluent/solver/fluent_modules/solution/methods.py
@@ -1,53 +1,20 @@
-class Methods():
-    """
-    Solver Settings
-│      ├── Pressure-velocity coupling
-│      ├── Spatial discretisation
-│      ├── Gradient
-│      └── Transient settings
-    """
-    def __init__(self) -> None:
-        pass
+class Methods:
+    def __init__(self):
+        self.session = None
 
-    def setup(self,session, config):
+    def setup(self, session, config):
         self.session = session
-        self.config = config
-
-        for name, value in config.items():
-
-            if value in ("", None, 0):
-                continue
-
-            method = getattr(self, f"set_{name}", None)
-
-            if method:
-                method(session, value)
+        discretization = config.get("discretization", {}) or {}
+        if discretization:
+            self.set_discretization(discretization)
+        return True
 
     def set_discretization(self, sub_config):
-        if "pressure" in sub_config:
-            self.session.tui.solve.set.discretization_scheme("pressure", 12)
-
-        """
-        below dependent on viscous model rather than what defined
-        - below doesnt matter, all will have to be second order
-        - will have to find specific commands to each viscous model such that 2nd discretization
-            is conserved
-        """
-        if "k-epislon" in self.config["viscous_model"]["model"]: 
-            self.session.tui.solve.set.discretization_scheme("k", sub_config["k"])
-            self.session.tui.solve.set.discretization_scheme("epsilon", sub_config["epilson"])
-        
-
-        """
-        # pythonic equivalent of 2nd order discretizations of above
-        self.session.solution.methods.spatial_discretization.discretization_scheme["pressure"] = "second-order"
-        self.session.solution.methods.spatial_discretization.discretization_scheme["k"] = "second-order-upwind"
-        self.session.solution.methods.spatial_discretization.discretization_scheme["epsilon"] = "second-order-upwind"
-        """
-
-        """
-        Further work:
-        - implement as a class for each viscous model as well as different characteristic (velocity etc)
-        """
-        
-
+        spatial = (
+            self.session.settings.solution.methods
+            .spatial_discretization.discretization_scheme
+        )
+        for field, value in sub_config.items():
+            if value not in ("", None, 0):
+                spatial[field] = value
+        return True
