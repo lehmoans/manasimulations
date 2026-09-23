2bdf4f55bc6e217b2b042ea70a4d976cbd47d383
sai <madanawas@gmail.com>
2026-09-24 04:21:32 +1000

    Integrate solution and post-processing config paths

--- autofluent/src/autofluent/solver/fluent_modules/post_process/contour.py
+++ autofluent/src/autofluent/solver/fluent_modules/post_process/contour.py
@@ -1,33 +1,21 @@
-from iso_surface import Iso_Surface
-
-class Contour():
-    def __init__(self,session,config) -> None:
+class Contour:
+    def __init__(self, session, config):
         self.session = session
-        self.sub_config = config["contour"]
-        
+        self.sub_config = config or {}
+
     def create(self):
-        for contour in self.sub_config:
-
-            contour_name = contour["name"]
-            contour_field = contour_name["field"]
-            contour_surf =contour["surface"]
-            contour_save_file_type = self.sub_config["save_format"]
-        
-            
-            graphics = self.session.settings.results.graphics
-            graphics.picture.driver_options.hardcopy_format = contour_save_file_type
-
-            graphics.contour[contour_name] = {
-                "field":contour_field ,
-                "surfaces_list": [contour_surf],
+        graphics = self.session.settings.results.graphics
+        for name, contour in self.sub_config.items():
+            if not contour:
+                continue
+            file_type = contour.get("save_format", "png")
+            graphics.picture.driver_options.hardcopy_format = file_type
+            graphics.contour[name] = {
+                "field": contour.get("field"),
+                "surfaces_list": contour.get("surfaces", []),
             }
-
             graphics.views.auto_scale()
-
             graphics.picture.save_picture(
-                file_name=f"{contour_name}.{contour_save_file_type}"
+                file_name=f"{name}.{file_type}"
             )
-            
-            
-
-
+        return True
