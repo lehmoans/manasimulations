1267c66a9b9b5d7e4aef8a4c09fe881e896a4759
sai <madanawas@gmail.com>
2026-09-23 15:10:50 +1000

    complete fixed Fluent setup workflow

--- autofluent/src/autofluent/solver/fluent_modules/setup/setup.py
+++ autofluent/src/autofluent/solver/fluent_modules/setup/setup.py
@@ -5,28 +5,40 @@ from .models import Models
 from .reference_values import Reference_Values
 from .zones import Zones
 
-class Setup():
-    def __init__(self) -> None:
-        
+
+class Setup:
+
+    def __init__(self):
         self.boundary_conditions = Boundary_Conditions()
         self.initialization = Initialisation()
         self.materials = Materials()
         self.viscous_models = Models()
         self.reference_values = Reference_Values()
         self.zones = Zones()
-    
+
     def setup(self, session, config):
-        self.viscous_models.setup(session, config["viscous_model"])
+        viscous_config = config.get("viscous_model")
+        if viscous_config:
+            self.viscous_models.setup(session, {"viscous_model": viscous_config})
 
-        self.materials.setup(session, config["materials"])
-        self.zones.setup(session, config[""]) # UPDATE THIS WITH FUNCTIONS 
+        materials_config = config.get("materials")
+        if materials_config:
+            self.materials.setup(session, materials_config)
 
-        self.reference_values = Reference_Values()
+        zones_config = config.get("zones")
+        if zones_config:
+            self.zones.setup(session, zones_config)
 
-        self.boundary_conditions = Boundary_Conditions()
-        self.initialization = Initialisation()
-        
-        
-        
-        
+        reference_config = config.get("reference_values")
+        if reference_config:
+            self.reference_values.setup(session, reference_config)
 
+        boundary_config = config.get("BC")
+        if boundary_config:
+            self.boundary_conditions.setup(session, boundary_config)
+
+        initialization_config = config.get("initialization")
+        if initialization_config:
+            self.initialization.setup(session, initialization_config)
+
+        return True
