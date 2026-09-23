4b888533df37998b04703ecd0d53cf7d442b850c
sai <madanawas@gmail.com>
2026-09-23 15:10:25 +1000

    align Fluent solver with fixed workflow interface

--- autofluent/src/autofluent/solver/fluent.py
+++ autofluent/src/autofluent/solver/fluent.py
@@ -1,32 +1,43 @@
 from .base import BaseSolver
-
 from .fluent_modules.solver import Solver
+
 
 class Fluent_Solver(BaseSolver):
 
     def __init__(self, session, config):
         super().__init__(config=config)
-
         self.fluent_solver = Solver(session, config)
-        
+        self.result = None
+        self.workflow = []
+
     def setup(self):
-        self.setup = self.fluent_solver.setup()
+        self.fluent_solver.setup()
+        self.workflow.append("setup")
+        return True
+
+    def setup_solution(self):
+        self.fluent_solver.setup_solution()
+        self.workflow.append("solution")
+        return True
 
     def solve(self):
-        self.solve = self.fluent_solver.solve()
+        self.result = self.fluent_solver.solve()
+        self.workflow.append("solve")
+        return self.result
 
     def post_process(self):
-        self.post_process = self.fluent_solver.post_process()
-    
+        self.fluent_solver.post_process()
+        self.workflow.append("post_process")
+        return True
+
     def save(self):
-        self.fluent_solver.save()
-    
+        output = self.fluent_solver.save()
+        self.workflow.append("save")
+        return output
+
     def run(self):
         self.setup()
+        self.setup_solution()
         self.solve()
         self.post_process()
-        self.save()
-    
-        
-
-   
+        return self.save()
