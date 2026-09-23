c603e1e50e334e18f451b00a9d8b198d7858c9ba
sai <madanawas@gmail.com>
2026-09-24 04:21:19 +1000

    Integrate solution and post-processing config paths

--- autofluent/src/autofluent/solver/fluent_modules/solution/monitors.py
+++ autofluent/src/autofluent/solver/fluent_modules/solution/monitors.py
@@ -1,48 +1,32 @@
-class Monitors():
-    def __init__ ( self):
-        pass
+class Monitors:
+    def __init__(self):
+        self.session = None
 
-    def setup(self,session, config):
+    def setup(self, session, config):
         self.session = session
-        self.config = config
+        residuals = config.get("residuals", {}) or {}
+        if residuals:
+            self.set_residuals(residuals)
 
-        for name, value in config.items():
+        reports = config.get("reports", {}) or {}
+        for name, report in reports.items():
+            if report:
+                self.set_report_monitor(name, report)
+        return True
 
-            if value in ("", None, 0):
+    def set_residuals(self, sub_config):
+        for equation, criteria in sub_config.items():
+            if criteria in ("", None, 0):
                 continue
+            self.session.settings.solution.monitor.residual.equations[
+                equation
+            ].absolute_criteria = criteria
 
-            method = getattr(self, f"set_{name}", None)
-
-            if method:
-                method( value)
-
-    def set_residals_criteria(self, sub_config):
-
-        self.session.settings.solution.monitor.residual.equations["continuity"].absolute_criteria = (
-            sub_config["continuity"]
-        )
-        self.session.settings.solution.monitor.residual.equations["x-velocity"].absolute_criteria = (
-            sub_config["x-velocity"]
-        )
-        self.session.settings.solution.monitor.residual.equations["y-velocity"].absolute_criteria = (
-            sub_config["y-velocity"]
-        )
-        self.session.settings.solution.monitor.residual.equations["z-velocity"].absolute_criteria = (
-            sub_config["z-velocity"]
-        )
-
-        if "k-epislon" in self.config["viscous_model"]["type"]:
-            self.session.settings.solution.monitor.residual.equations["k"].absolute_criteria = sub_config["k-velocity"]
-            self.session.settings.solution.monitor.residual.equations["epsilon"].absolute_criteria = (
-                sub_config["epislon"])
-
-    def set_report_monitors(self, sub_config):
-        self.session.settings.solution.monitor.report_plots.create(name= sub_config["name"])
-        self.session.settings.solution.monitor.report_plots[sub_config["name"]] = {"report_defs": [sub_config["parameter"]]}
-
-    
-    """
-    setup for other models
-    - possible relate to the viscous model where parameters defined there will determine parameters here
-    """
-    
+    def set_report_monitor(self, name, sub_config):
+        parameter = sub_config.get("parameter")
+        if not parameter:
+            return
+        self.session.settings.solution.monitor.report_plots.create(name=name)
+        self.session.settings.solution.monitor.report_plots[name] = {
+            "report_defs": [parameter]
+        }
